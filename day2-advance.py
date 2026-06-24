8b76b8f787113df7a332e4aff8dd4d754e9719eb
ShirakoSora <2602291742@qq.com>
2026-06-24 14:58:53 +0800

    day2-advanced: logical operator and multi-condition roller coaster access judgment

---
+++ day2-advance.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age:"))
+height = int(input("Enter your height(cm)"))
+
+if age >= 16 and height >= 150:
+    print("Allowed to ride roller coaster")
+else:
+    print("Not allowed to ride roller coaster")
+    
